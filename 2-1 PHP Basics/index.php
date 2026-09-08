ff31e08af247acac925af232ace4f8aba82b108f
Eric Burkheimer <eaburkheimer@dmacc.edu>
2026-09-08 14:14:01 -0500

    git commit test

--- 2-1 PHP Basics/index.php
+++ 2-1 PHP Basics/index.php
@@ -3,9 +3,32 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP Basics</title>
+  <style>
+    .color-green {
+      background-color: green;
+      border: 1px solid;
+    }
+
+    .color-red {
+      background-color: red;
+      border: 1px solid;
+    }
+  </style>
 </head>
 <body>
+  <?php
+    echo "<h3>Hello World!</h3>";
 
+    $myArray = ['apples', 'oranges', 'grapes'];
+
+    foreach($myArray as $value) {
+  ?>
+    <p>
+      <?php echo $value; ?>
+    </p>
+  <?php
+    }
+  ?>
 </body>
 </html>
